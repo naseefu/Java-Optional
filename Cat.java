a51a8e710b0ea9ba1f7edd1b10b3302d4832857c
naseefu <naseefrahman90@gmail.com>
2024-12-31 13:18:15 +0530

    pull

--- Cat.java
+++ Cat.java
@@ -1,27 +1,4 @@
-public class Cat {
+public record Cat(String name, int age) {
 
-    public Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
 
-    private String name;
-
-    private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
